a0f7a0af10cf71b82233ad9c36a6168ab4bc267d
Romain Sanchez <romainsancho@gmail.com>
2016-06-21 14:35:01 +0200

    added missing translations

---
+++ Tests/Entity/StrainTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Librinfo\VarietiesBundle\Tests\Entity;
+
+use Librinfo\VarietiesBundle\Entity\Strain;
+
+class DefaultControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(1 == 1);
+    }
+}
