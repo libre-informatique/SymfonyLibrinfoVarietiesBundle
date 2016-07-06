073481e99f3c9a83df762f07dc61b80ac1cc82b8
Romain Sanchez <romainsancho@gmail.com>
2016-07-06 17:47:36 +0200

    missing translations

---
+++ Form/Type/CustomChoiceType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Librinfo\VarietiesBundle\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Librinfo\CoreBundle\Form\AbstractType as BaseAbstractType;
+use Librinfo\VarietiesBundle\Entity\LifeCycle;
+
+class LifeCycleType extends BaseAbstractType
+{
+    public function getParent() {
+        return 'choice';
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $choices = [];
+        foreach ( LifeCycle::values() as $lifecycle )
+            $choices[$lifecycle] = 'librinfo.label.life_cycle_' . $lifecycle;
+        $resolver->setDefaults([
+            'multiple' => false,
+            'expanded' => false,
+            'choices' => $choices
+        ]);
+    }
+}
