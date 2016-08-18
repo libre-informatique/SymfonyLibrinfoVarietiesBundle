920a2cf1a17b9d690a93974d9e838ba332d2cf4f
Romain Sanchez <romainsancho@gmail.com>
2016-08-18 15:55:06 +0200

    Regenerated admin classes for SelectChoices -  refs #1798

---
+++ Admin/SelectChoiceAdminConcrete.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Librinfo\VarietiesBundle\Admin;
+
+use Librinfo\CoreBundle\Admin\Traits\EmbeddedAdmin;
+
+class SelectChoiceAdminConcrete extends SelectChoiceAdmin
+{
+    use EmbeddedAdmin;
+}
