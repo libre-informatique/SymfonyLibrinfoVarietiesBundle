c9e88fbe12701eb7b5f081e0ac3f019cc1cf2782
Romain Sanchez <romainsancho@gmail.com>
2016-09-08 17:34:39 +0200

    Added action to create a strain from a variety - refs #1801

--- Controller/VarietyCRUDController.php
+++ Controller/VarietyCRUDController.php
@@ -1,12 +1,23 @@
 <?php
+
 namespace Librinfo\VarietiesBundle\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Sonata\AdminBundle\Controller\CRUDController;
+use Librinfo\CoreBundle\Controller\CRUDController;
 
 class VarietyCRUDController extends CRUDController
 {
+
+    public function strainAction()
+    {
+        $id = $this->getRequest()->get($this->admin->getIdParameter());
+        $object = clone $this->admin->getObject($id);
+        $object->setIsStrain(true);
+        
+        return $this->createAction($object);
+    }
+    
     /**
      *
      * @param String $fieldName the name of the field to get the form widget for
@@ -17,54 +28,66 @@ class VarietyCRUDController extends CRUDController
         $translator = $this->get('translator');
         $fieldSet = $request->get('fieldset');
         $field = $request->get('field');
-       
+
         $config = $this->admin->getConfigurationPool()->getContainer()->getParameter('librinfo_varieties');
         $fieldConfig = $config['variety_descriptions'][$fieldSet][$field];
         $choiceType = 'librinfo_customchoice';
         $options = $fieldConfig['options'];
         $type = $fieldConfig['type'] != 'textarea' ? $fieldConfig['type'] : 'text';
-        
-        if($type == 'librinfo_customcheckbox')
+
+        if ( $type == 'librinfo_customcheckbox' )
         {
             $type = 'choice';
             $options = array_merge($options, array(
                 'choices' => array('1' => $translator->trans('yes'), '0' => $translator->trans('no'))
-                ));
+            ));
         }
-        
-        if($type == $choiceType)
+
+        if ( $type == $choiceType )
         {
             $options['is_filter'] = true;
-            
-            if(isset($options['expanded']))
+
+            if ( isset($options['expanded']) )
                 unset($options['expanded']);
-            
-            if(isset($options['multiple']))
+
+            if ( isset($options['multiple']) )
                 unset($options['multiple']);
         }
-            
-        if (isset($options['choices']) && empty($options['choices']))
+
+        if ( $type == 'librinfo_tinymce' )
+            $type = 'text';
+
+        if ( isset($options['choices']) && empty($options['choices']) )
             unset($options['choices']);
-        
-        if (isset($options['choices_class']) && $type != $choiceType)
+
+        if ( isset($options['choices_class']) && $type != $choiceType )
             unset($options['choices_class']);
-        
-        if (isset($options['librinfo_choices']) && $type != $choiceType)
+
+        if ( isset($options['librinfo_choices']) && $type != $choiceType )
             unset($options['librinfo_choices']);
-        
-        if (isset($options['help']))
+
+        if ( isset($options['help']) )
             unset($options['help']);
-        
+
         $view = $this->createFormBuilder()
-                    ->add('value', $type, $options)
-                    ->getForm()
-                    ->createView()
-                ;   
-        dump($this->admin->getDatagrid());
-        dump($this->admin->getDatagridBuilder());
-        
+                ->add('value', $type, $options)
+                ->getForm()
+                ->createView()
+        ;
+
         return $this->render('LibrinfoVarietiesBundle:Form:filter_widget.html.twig', array(
-            'form' => $view
-        ), null);
+                    'form' => $view
+                        ), null);
     }
+
+    protected function preCreate(Request $request, $object)
+    {
+        if ( $this->isXmlHttpRequest() )
+        {
+            $variety = $this->admin->getObject($request->get('parentId'));
+            $object->setParent($variety);
+            $object->setIsStrain(true);
+        }
+    }
+
 }
