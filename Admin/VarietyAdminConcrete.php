0e765cefdff3dcccc844215e89e43fca12eb6432
Romain Sanchez <romainsancho@gmail.com>
2016-09-23 16:46:05 +0200

    Added prepersist check to prevent variety being persisted with its own id as parent_id

--- Admin/VarietyAdminConcrete.php
+++ Admin/VarietyAdminConcrete.php
@@ -4,6 +4,7 @@ namespace Librinfo\VarietiesBundle\Admin;
 
 use Symfony\Component\Form\FormEvents;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Librinfo\CoreBundle\Admin\Traits\HandlesRelationsAdmin;
 use Librinfo\VarietiesBundle\Traits\DynamicDescriptions;
@@ -13,9 +14,10 @@ class VarietyAdminConcrete extends VarietyAdmin
 
     use HandlesRelationsAdmin {
         configureFormFields as configFormHandlesRelations;
+        configureShowFields as configShowHandlesRelations;
     }
 
-use DynamicDescriptions;
+    use DynamicDescriptions;
 
     /**
      * Configure routes for list actions
@@ -28,13 +30,21 @@ use DynamicDescriptions;
         $collection->add('strain');
     }
 
+    /**
+     * Configure create/edit form fields 
+     * 
+     * @param FormMapper 
+     */
     protected function configureFormFields(FormMapper $mapper)
     {
+        //calls to methods of traits
         $this->configFormHandlesRelations($mapper);
         $this->configureDynamicDescriptions($mapper);
-
+        
+        // $this into a variable to use it in closure
         $admin = $this;
-
+        
+        //Form event listener to remove children form tab if object is a strain
         $mapper->getFormBuilder()->addEventListener(FormEvents::POST_SET_DATA, function ($event) use ($admin, $mapper) {
             $subject = $admin->getSubject($event->getData());
             $form = $event->getForm();
@@ -47,13 +57,16 @@ use DynamicDescriptions;
                 $form->remove('children');
             }
         });
+ 
     }
-
-    public function getFormTheme()
+    
+    //prevent primary key loop
+    public function prePersist($variety)
     {
-        return array_merge(
-                parent::getFormTheme(), array('LibrinfoVarietiesBundle:Form:form_admin_fields.html.twig')
-        );
+        parent::prePersist($variety);
+        
+        if($variety->getParent()->getId() == $variety->getId())
+            $variety->setId(NULL);
     }
-
+    
 }
