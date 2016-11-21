71e662cdecfb7de9ac0b629b75f67f49fc0d1d65
Romain <romain.sanchez@libre-informatique.fr>
2016-11-21 14:47:18 +0100

    OuterExtension refactoring

--- Entity/Variety.php
+++ Entity/Variety.php
@@ -2,7 +2,7 @@
 
 namespace Librinfo\VarietiesBundle\Entity;
 
-use AppBundle\Entity\Extension\VarietyExtension;
+use AppBundle\Entity\OuterExtension\LibrinfoVarietiesBundle\VarietyExtension;
 use Librinfo\OuterExtensionBundle\Entity\Traits\OuterExtensible;
 use Doctrine\Common\Collections\ArrayCollection;
 use Librinfo\DoctrineBundle\Entity\Traits\Nameable;
