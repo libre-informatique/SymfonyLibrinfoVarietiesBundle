95b8acc9f6e107b066297f56292dc379e4b4f7a3
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2017-03-16 10:04:02 +0100

    Use form types FQDN instead of name (fixes deprecation for sf>2.8)

--- Admin/VarietyDescriptionAdmin.php
+++ Admin/VarietyDescriptionAdmin.php
@@ -1,15 +1,24 @@
 <?php
 
+/*
+ * Copyright (C) 2015-2017 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Librinfo\VarietiesBundle\Admin;
 
-use Sonata\AdminBundle\Form\FormMapper;
 use Blast\CoreBundle\Admin\CoreAdmin;
 use Blast\CoreBundle\Admin\Traits\EmbeddedAdmin;
+use Blast\UtilsBundle\Form\Type\CustomChoiceType;
+use Sonata\AdminBundle\Form\FormMapper;
 
 class VarietyDescriptionAdmin extends CoreAdmin
 {
     use EmbeddedAdmin;
-    
+
     /**
      * @param FormMapper $formMapper
      */
@@ -19,19 +28,19 @@ class VarietyDescriptionAdmin extends CoreAdmin
         $fieldset = $this->subject->getFieldset();
         $field = $this->subject->getField();
         $config = empty($vd_config[$fieldset][$field]) ? '' : $vd_config[$fieldset][$field];
-        $type = isset($config['type']) ? $config['type'] : 'textarea'; 
-        $choiceType = 'blast_custom_choice';
+        $type = isset($config['type']) ? $config['type'] : 'textarea';
+        $choiceType = CustomChoiceType::class;
         $options = empty($config['options']) ? [] : $config['options'];
-        
+
         if (isset($options['choices']) && empty($options['choices']))
             unset($options['choices']);
-        
+
         if (isset($options['choices_class']) && $type != $choiceType)
             unset($options['choices_class']);
-        
+
         if (isset($options['blast_choices']) && $type != $choiceType)
             unset($options['blast_choices']);
-        
+
         if (!isset($options['label']) || !$options['label'])
             $options['label'] = sprintf("librinfo_description_%s_%s", $fieldset, $field);
 
