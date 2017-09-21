9544c98a6054282755fddbe78d5ce78e66122a2a
PapsOu <vivien.frasca@libre-informatique.fr>
2017-09-21 18:24:07 +0200

    Use twig function for variety description label and value

--- src/Twig/VarietyDescription.php
+++ src/Twig/VarietyDescription.php
@@ -12,16 +12,39 @@
 
 namespace Librinfo\VarietiesBundle\Twig;
 
+use Symfony\Component\Translation\TranslatorInterface;
+
 class VarietyDescription extends \Twig_Extension
 {
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @var array
+     */
+    private $varietyConfig;
+
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction('displayVarietyDescription', [$this, 'displayVarietyDescription'], ['is_safe'=>['html']]),
+            new \Twig_SimpleFunction('displayVarietyDescriptionLabel', [$this, 'displayVarietyDescriptionLabel'], ['is_safe'=>['html']]),
+            new \Twig_SimpleFunction('displayVarietyDescriptionValue', [$this, 'displayVarietyDescriptionValue'], ['is_safe'=>['html']]),
         ];
     }
 
-    public function displayVarietyDescription($varietyDescription)
+    public function displayVarietyDescriptionLabel($varietyDescription)
+    {
+        return $this->displayVarietyDescription($varietyDescription, true);
+    }
+
+    public function displayVarietyDescriptionValue($varietyDescription)
+    {
+        return $this->displayVarietyDescription($varietyDescription, false);
+    }
+
+    public function displayVarietyDescription($varietyDescription, $label = false)
     {
         $fieldSet = $varietyDescription->getFieldset();
         $field = $varietyDescription->getField();
@@ -33,11 +56,30 @@ class VarietyDescription extends \Twig_Extension
             $configFieldset = $config[$fieldSet];
         }
 
-        return $value;
+        if ($label) {
+            $help = $this->translator->trans('librinfo.help.' . $field, [], 'messages');
+            $label = $this->translator->trans($field, [], 'messages');
+
+            if ($help != ' ') {
+                $label .= ' <small>(' . $help . ')</small>';
+            }
+
+            return $label;
+        } else {
+            return $value;
+        }
     }
 
     public function setVarietyConfig($config)
     {
         $this->varietyConfig = $config;
+    }
+
+    /**
+     * @param TranslatorInterface translator
+     */
+    public function setTranslator(TranslatorInterface $translator): void
+    {
+        $this->translator = $translator;
     }
 }
